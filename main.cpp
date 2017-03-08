a1ceedc4d585d9ecd42e3dfce0a66a58128c70e6
Alexis <alexis.st-onge@polymtl.ca>
2017-03-08 16:58:00 -0500

    modified the project config for visual studio to have the right program arguments

--- main.cpp
+++ main.cpp
@@ -177,7 +177,7 @@ std::list<int> Graph::get_longest_chain()
 
 int main(int argc, char **argv)
 {
-	std::cout << "Working directory as seen by the program : " << getenv("PWD") << std::endl;
+	//std::cout << "Working directory as seen by the program : " << system("cd") << std::endl;
 	if (argc < 2){
 		std::cout << "Usage " << argv[0] << " filename" << std::endl;
 		return 1;

20392b5f029f7bdc910fed59813735ffebe0e5a5
Philippe Carphin <philippe.carphin@polymtl.ca>
2017-03-19 13:23:22 -0400

    Added backtrack calculator of number of linear extensions
    Tested by comparing the output with the correct answer on moodle

---
+++ backtrack.py
@@ -0,0 +1,25 @@
+from lazy_DAG import lazy_DAG
+
+class Backtrack(object):
+    def __init__(self):
+        self.count = 0
+    def __call__(self,graph):
+        self.count = 0
+        self.__backtrack(graph)
+        return self.count
+    def __backtrack(self, graph):
+        if graph.empty():
+            self.count += 1
+            return
+        for u in graph.in_degree_0():
+            graph.remove_node(u)
+            self.__backtrack(graph)
+            graph.add_node(u)
+
+if __name__ == "__main__":
+    ld = lazy_DAG("./tp2-donnees/poset10-4a")
+    print(ld.adj_dict)
+    b = Backtrack()
+    print("Number of linear extensions using backtrack {}".format(b(ld)))
+
+

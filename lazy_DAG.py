6d2e64c809c1bec12650736c0e493367068cf6e5
Philippe Carphin <philippe.carphin@polymtl.ca>
2017-03-23 09:10:16 -0400

    More documentation and added possibility to get the transitive closure as a different object
    Transitive closure creates a copy of the current graph, transitively closes
    that copy and returns it.

--- lazy_DAG.py
+++ lazy_DAG.py
@@ -38,6 +38,10 @@ class lazy_DAG(object):
             for node in ordered_dict:
                 self.n_nodes += 1
                 self.n_edges += len(ordered_dict[node])
+        elif lazy_dag != None:
+            self.n_nodes = lazy_dag.n_nodes
+            self.n_edges = lazy_dag.n_edges
+            self.adj_dict = OrderedDict(lazy_dag.adj_dict)
         else:
             raise exception
 
@@ -82,51 +86,46 @@ class lazy_DAG(object):
             self.active_nodes += 1
 
     def add_edge(self, u,v):
-        self.adj_dict[u].add(v)
+        """ Adds an edge u -> v to the graph """
+        if v not in self.adj_dict[u]:
+            self.n_edges += 1
+            self.adj_dict[u].add(v)
 
     def has_edge(self, u, v):
-        assert (self._is_active(u) and self._is_active(v))
+        """ Checks if the graph has an edge u -> v, this function must be called
+        with nodes that have not been removed """
+        assert (self._is_active(u) and self._is_active(v)), "has_edge cannot be called on nodes that have been removed"
         assert u < self.n_nodes and v < self.n_nodes
         return v in self.adj_dict[u]
 
     def nodes(self):
+        """ Returns a list of active nodes in the graph """
         return [v for v in range(self.n_nodes) if self._is_active(v)]
 
     def successors(self, u):
-        assert self._is_active(u)
+        """ Returns the list of successors of the node u in the graph.  This
+        function must be called with a node that has not been removed """
+        assert self._is_active(u), "successors cannot be called on a node that has been removed"
         return [v for v in self.adj_dict[u] if self._is_active(v)]
 
     def predecessors(self, v):
-        assert self._is_active(v)
+        """ Returns the list of predecessors of v.  This function must be called
+        with a node that has not been removed."""
+        assert self._is_active(v), "predecessors cannot be called on a node that has been removed"
         return [u for u in self.nodes() if self.has_edge(u,v)]
 
     def in_degree(self, v):
+        """ Returns the in_degree of a node: the number of edges coming into the
+        node = the number of predecessors """
+        assert self._is_active(v), "in_degree cannot be called with a node that has been removed"
         return len(self.predecessors(v))
 
     def in_degree_0(self):
+        """ Returns a list of nodes that do not have any predecessors """
         return [v for v in self.nodes() if self.in_degree(v) == 0]
 
-    def adj_mat(self):
-        mat = np.zeros((self.n_nodes, self.n_nodes), dtype=bool)
-        for u in self.adj_dict:
-            for v in self.adj_dict[u]:
-                mat[u,v] = 1
-        return mat
-    def transitive_close_mat(self, mat):
-        for k in range(self.n_nodes):
-            for i in range(self.n_nodes):
-                for j in range(self.n_nodes):
-                    mat[i,j] = mat[i,j] or (mat[i,k] and mat[k,j])
-    def adj_mat_to_adj_dict(self, mat):
-        new_dict = OrderedDict()
-        for u in range(self.n_nodes):
-            new_dict[u] = set()
-        for u in range(self.n_nodes):
-            for v in range(self.n_nodes):
-                if mat[u,v]:
-                    new_dict[u].add(v)
-        print(new_dict)
-    def transitive_closure(self):
+    def transitive_close(self):
+        """ Transitively closes a graph """
         for u in self.adj_dict:
             nodes_seen = []
             node_queue = deque(self.adj_dict[u])
@@ -135,6 +134,11 @@ class lazy_DAG(object):
                 nodes_seen.append(v)
                 self.add_edge(u,v)
                 node_queue += [v for v in self.adj_dict[v] if v not in nodes_seen]
+
+    def transitive_closure(self):
+        new_graph = lazy_DAG(lazy_dag=self)
+        new_graph.transitive_close()
+        return new_graph
 
 if __name__ == "__main__":
     ld = lazy_DAG("./tp2-donnees/poset10-4a")
@@ -146,8 +150,7 @@ if __name__ == "__main__":
     print("in_degree_0(): after removing 9")
     print(ld.in_degree_0())
     ld.add_node(9)
-    mat = ld.adj_mat()
-    ld.adj_mat_to_adj_dict(mat)
-    ld.transitive_closure()
+    ld_close = ld.transitive_closure()
+    print(ld_close.adj_dict)
 
 
